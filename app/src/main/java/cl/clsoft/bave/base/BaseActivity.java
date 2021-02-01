40735aa3a1e31da98de2c600949b6e5db6003046
Cristian Fuentes A <cfuentes@iodev.cl>
2021-02-01 04:27:35 -0300

    Refactoring Dao

--- app/src/main/java/cl/clsoft/bave/base/BaseActivity.java
+++ app/src/main/java/cl/clsoft/bave/base/BaseActivity.java
@@ -3,17 +3,22 @@ package cl.clsoft.bave.base;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import cl.clsoft.bave.R;
 import cn.pedant.SweetAlert.SweetAlertDialog;
 
 public abstract class BaseActivity<Presenter extends BasePresenter> extends AppCompatActivity {
 
     protected Presenter mPresenter;
     protected static final String APP = "INVENTARIO";
+    protected View llProgressBar;
+    protected TextView pbText;
 
     @NonNull
     protected abstract Presenter createPresenter(@NonNull final Context context);
@@ -52,6 +57,29 @@ public abstract class BaseActivity<Presenter extends BasePresenter> extends AppC
                                            @NonNull final int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
         mPresenter.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
+    public void showProgres() {
+        if (this.llProgressBar != null){
+            TextView tvMensaje = this.llProgressBar.findViewById(R.id.pbText);
+            tvMensaje.setText("Por favor espere ...");
+            this.llProgressBar.setVisibility(View.VISIBLE);
+
+        }
+    }
+
+    public void showProgres(String mensaje) {
+        if (this.llProgressBar != null){
+            TextView tvMensaje = this.llProgressBar.findViewById(R.id.pbText);
+            tvMensaje.setText(mensaje);
+            this.llProgressBar.setVisibility(View.VISIBLE);
+
+        }
+    }
+
+    public void hideProgres() {
+        if (this.llProgressBar != null)
+            this.llProgressBar.setVisibility(View.GONE);
     }
 
     public void showWarning(String mensaje) {
